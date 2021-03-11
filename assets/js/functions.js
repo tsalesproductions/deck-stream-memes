1dfd6c1ff0774cefcd40f2cbdb1092b04a03e780
Thiago Sales <thiago_salests@hotmail.com>
2021-03-11 03:18:14 -0300

    final project

---
+++ assets/js/functions.js
@@ -0,0 +1,293 @@
+const audios = [
+    {
+        nome: "Yamete Kudasai",
+        src: "assets/audios/asus-yamete-kudasai.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Achou que eu tava brincando?",
+        src: "assets/audios/achoqueeutavabrincando.mp3",
+        keyCode: "Numpad4"
+    },
+    {
+        nome: "Agora eu entendi",
+        src: "assets/audios/agoraeuentendi.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Aham, sei",
+        src: "assets/audios/ahamsei.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Aqiui tem coragem",
+        src: "assets/audios/aquitemcoragem.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Arreda o pé daqui",
+        src: "assets/audios/arredadaqui.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Arregou",
+        src: "assets/audios/arregou.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Azideia",
+        src: "assets/audios/azideia.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Carai borracha",
+        src: "assets/audios/caraiborracha.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Cê é o bichão mesmo hein",
+        src: "assets/audios/ceehobichaomesmo.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Como assim?",
+        src: "assets/audios/comoassim.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Como você é burro",
+        src: "assets/audios/comovceburro.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "é, ta bom",
+        src: "assets/audios/ehtabom.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "É uma cilada bino",
+        src: "assets/audios/ehumaciladabino.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "É Verdade",
+        src: "assets/audios/ehverdade.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "É mesmo é?",
+        src: "assets/audios/emesmoe.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Esquece essa merda ai",
+        src: "assets/audios/esqueceessamerdaai.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Eu não quero",
+        src: "assets/audios/eunaoquero.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Pergunta Besta",
+        src: "assets/audios/fazerperguntabesta.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Baguncinha",
+        src: "assets/audios/fizumabaguncinha.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Fui tapeado",
+        src: "assets/audios/fuitapeado.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Encher o saco",
+        src: "assets/audios/gostamencher.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Boiola",
+        src: "assets/audios/humboiola.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Já avisei que vai dar merda",
+        src: "assets/audios/jaaviseiquevaidarmerda.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Mano, cê tem demência?",
+        src: "assets/audios/manocetemdemencia.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Melhor Vigarice",
+        src: "assets/audios/melhorvigarice.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Não consegue né?",
+        src: "assets/audios/naoconseguene.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Não entendi foi nada",
+        src: "assets/audios/naoentendifoinada.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Não",
+        src: "assets/audios/naoseumadruga.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Não venha com essas perguntas",
+        src: "assets/audios/naovenhacomessaspergunta.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Oi",
+        src: "assets/audios/oioicaindo.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "O miserável é um gênio",
+        src: "assets/audios/omiseravelehumgenio.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "O que? Como?",
+        src: "assets/audios/oquecomo.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Ou será que não",
+        src: "assets/audios/ouseraquenao.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Que cara mais engraçado",
+        src: "assets/audios/quecaramaisengracado.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Sabia não",
+        src: "assets/audios/quemerdaemsabianao.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Risada",
+        src: "assets/audios/risadacarlosalberto.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Erro",
+        src: "assets/audios/somerrowindows.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Grilo",
+        src: "assets/audios/somgrilo.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Sopa de macaco",
+        src: "assets/audios/sopademacaco.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Sou um merda",
+        src: "assets/audios/souummerdamerdao.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Talvez não funcione com burros",
+        src: "assets/audios/talveznaofuncionecomburros.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Cagado de fome",
+        src: "assets/audios/tocagadodefome.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Tô lascado",
+        src: "assets/audios/tolascado.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Tudo errado essas pergunta",
+        src: "assets/audios/tudoerradoessaspergunta.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Tu é gay?",
+        src: "assets/audios/tuehga.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Vai te lascar",
+        src: "assets/audios/vaitelascararrombado.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Você é retardado por acaso?",
+        src: "assets/audios/voceehretardadoporacaso.mp3",
+        keyCode: ""
+    },
+    {
+        nome: "Wowuau",
+        src: "assets/audios/wowuau.mp3",
+        keyCode: ""
+    }
+];
+
+deckManager = {
+    target: null,
+    createDiv: function(item){
+        let div = document.createElement("div");
+            div.classList.add("item");
+
+        div.addEventListener("click", function(){
+            let audio = new Audio(item.src);
+                audio.currentTime = 0;
+                audio.play();
+        });
+
+        let span = document.createElement("span");
+            span.innerText = item.nome;
+
+        div.append(span);
+
+        this.target.append(div);
+    },
+    listAudio: function(){
+        audios.map(function(item){
+            deckManager.createDiv(item); 
+        });
+    },
+    mappingKeyBoard: function(event){
+        audios.map(function(item){
+            let depurator = document.querySelector(".keyPress");
+            if(event.code == item.keyCode){
+                let audio = new Audio(item.src);
+                    audio.currentTime = 0;
+                    audio.play();
+            }
+            
+            depurator.innerText = event.code;
+        });
+    },
+    init: function(){
+        this.target = document.querySelector(".container");
+        this.listAudio();
+    }
+}
+
+window.addEventListener("load", deckManager.init());
+
+document.addEventListener("keypress", (event) => deckManager.mappingKeyBoard(event));
